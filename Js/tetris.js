d1a590c7e3b47a48780d9dd3d0d84de14ee12e9e
DaniKPosada <d.posada1044@gmail.com>
2022-11-21 13:49:34 -0500

    comentarios en ingles

--- Js/tetris.js
+++ Js/tetris.js
@@ -1,7 +1,7 @@
-let lastTime = 0;/*paso 9: la variable creada se inicializa en 0 */
+let lastTime = 0;/* the created variable is initialized to 0 */
 
-let dropInterval = 1000;//variable for regular la caída de la ficha 
-let dropCounter= 0; //variable para regular la caída de la ficha
+let dropInterval = 1000;//Variable to regulate the drop of the token
+let dropCounter= 0; //Variable to regulate the drop of the token
 
 
 let movLeft = document.getElementById("arrowLeft");
@@ -9,18 +9,18 @@ let movRight = document.getElementById("arrowRight");
 let movUp = document.getElementById("arrowUp");
 let movDown = document.getElementById("arrowDown");
 
-const CANVAS = document.getElementById("tetris"); /* paso 3: se trae el canvas de html */
-const CONTEXT = CANVAS.getContext("2d"); /* paso 4: Se crea una variable que establezca la visualización del canvas en 2d  */
+const CANVAS = document.getElementById("tetris"); /* brings the canvas from HTML file */
+const CONTEXT = CANVAS.getContext("2d"); /* It creates a variable that alows us stablish the visualization of the canvas in 2d */
 
 const canvasNext = document.getElementById("nextPiece");
 const contextNext = canvasNext.getContext("2d");
 
-const grid = createMatriz(10,20);/*Variable // funciona aninima para crear una tabla de 20 filas por 10 columnas -- se utilizan los valores de create matriz 10,20 como parametro en la funcion createMatriz */
+const grid = createMatriz(10,20);/*Variable // Animated function to create a table of 20 rows for ten columns -- the values 10,20 are used as parameters in the createMatriz */
 
-const color1= "rgb(255,255,0)"; // TODO DEFINIR COLORES DE LAS DEMAS PIEZAS 
+const color1= "rgb(255,255,0)"; // Define one unic color for all de tokens (TODO give to each token a color) 
 
-const colors = [ // variable para definir los colores, después se cita en draw matriz 
-    null, //Ya que el 0 esta reservado a las posiciones nulas de la tabla, se asigna valor null a 0  
+const colors = [ // This is a variable to define the colors, it is used in the array draw matriz 
+    null, //We want make this value null because we want to start from 1 and not from 0
     color1,
     color1,
     color1,
@@ -30,23 +30,23 @@ const colors = [ // variable para definir los colores, después se cita en draw 
     'pink'
 ];
 
-const player ={/*objeto llave y valor  pos abarca la posicion donde se ubica el tetromino*/
+const player ={/*The objet "player" have a key and a value named pos that has the position where the tetromino is alocated*/
     pos: {x:0, y:0},
-    matriz: null, //al comienzo se creaba la ficha aqui, pero cuando se integran las demás, se almacenan en la función createPiece
+    matriz: null, //at the beginning the piece was created here, but when the others are integrated, they are stored in the createPiece function
     next:null,
     score:0,
     level:0,
     lines:0   
 };
 
-CONTEXT.scale(30,30); /*paso 5: Se establecen las dimensiones del tablero canvas teniendo en cuenta el height y el weight del html  */
+CONTEXT.scale(30,30); /*The dimensions of the canvas board are established taking into account the height and weight of the html */
 
-contextNext.scale (40,40); // Escala del canvas para la visualización de la siguiente ficha que va a salir. 
+contextNext.scale (40,40); // CAnva's for the visualization of the following token that will come. 
 
 
 function createPiece (tipo){
     if(tipo==='T') {
-        /*array bidimensional que modifica en  x y y para difujar la ficha */
+        /*array towdimensional array that modifies in  x and y for draw the token*/
         return [ 
                 [0,0,0],
                 [1,1,1],
@@ -90,11 +90,10 @@ function createPiece (tipo){
          ];
     }      
 }
-
-
-function createMatriz (width,height){/*se crea una funcion que trae como parametros los vbalores de la variable definida al comienzo */
-    const matriz = [];/*se crea un array en blanco  */
-    while (height--){/*height corresponde a 20, recordar que los numeros siempre van a ser un operador bnoleano  TRUE si se pasan como unico parametro dentro del condicional se va a ejecturar, por ende, 20 veces se imprime un array en la posición width o la fila con #0  */
+//Create the canvas  with values of width y height tha will be specified then
+function createMatriz (width,height){/*We create a function that brings as parameters the values of the variable definited to the beginning*/
+    const matriz = [];/*We create an empty array */
+    while (height--){/*height is 20, remember that the numbers always will be a no boolean operator recordar que los numeros siempre van a ser un operador bnoleano  TRUE si se pasan como unico parametro dentro del condicional se va a ejecturar, por ende, 20 veces se imprime un array en la posición width o la fila con #0  */
         matriz.push(new Array (width).fill(0));
     }
     console.table(matriz) /*Se imprime una tabla .table que muestre la matriz del comienzo que se ha modificado por medio del while  */
@@ -103,7 +102,7 @@ function createMatriz (width,height){/*se crea una funcion que trae como paramet
 /*recibe el grid por l matriz y el player tiene la matriz de lka ficha */
 
 
-
+//función para colisiones entre la pieza y los bordes del tablero u otra pieza
 function collide (grid, player){ //funcion para limitar los movimientos dentro del canvas para que las figuras no superen los limites INFERIORES
 
     const matriz = player.matriz; //Variable definida para obtener la ruta de acceso directo a la lisya del objeto
@@ -120,7 +119,7 @@ function collide (grid, player){ //funcion para limitar los movimientos dentro d
     }
     return false; //retorna que no hay colision 
 }
-
+//función para colisiones entre la pieza y otra pieza
 function merge (grid,player) { //funcion creada para que ua ficha no pase por encima de otra sino que se agrupen
     player.matriz.forEach((row, y) => {
         row.forEach((value, x) => {
@@ -130,7 +129,7 @@ function merge (grid,player) { //funcion creada para que ua ficha no pase por en
         });
     });      
 }
-
+//dibuja la matriz, para mostrarla en el canvas
 function drawMatriz (matriz, offset) {/*Esta funcion trabaja con los parametros definidos dentro de la funcion draw  */
     /*array function funciona para abreviar la declaracion de una funcion y no poner las palabras reservadas de de las funciones como el function o el return*/
     matriz.forEach((row, y) => { /* el primer for each recibe dos parametros creados momentaneamente encargados de:  (row| se encarga de alamacenar los elementos de la lista matriz [0,0,0],[1,1,1],[0,1,0]) (y| se encarga de almacenar la posicion del elemento dentro de la lista:  # 0 -posicion- [0,0,0], # 1 [1,1,1], #2 [0,1,0]) */
@@ -196,7 +195,7 @@ function gridSweep () {//Funcion que elimina las lineas completadas
             }              
         }
 }
-
+//actualiza el puntaje
 function updateScore () {
     document.getElementById("score").innerHTML = player.score;
     document.getElementById("level").innerHTML = player.level;
@@ -207,13 +206,13 @@ function updateScore () {
 
 
 
-function update(time = 0) {/*paso 8: Se crea una función que se ejecute para inicializar el juego */
+function update(time = 0) {/* Se crea una función que se ejecute para inicializar el juego */
 
-    const deltaTime = time - lastTime;/* paso 9: se declara una variable constante que juegue con los parametros de ingreso de update y se declara una variable global lasttime⏫*/
+    const deltaTime = time - lastTime;/* se declara una variable constante que juegue con los parametros de ingreso de update y se declara una variable global lasttime⏫*/
     lastTime = time;  
 
     /*¿CÓMO SE MUEVE LA FICHA? */
-    dropCounter += deltaTime; //despues de definir la variable dropcounter arriba, se genera una oparción que se repetirá siempre debido al request animation de abajo⏬
+    dropCounter += deltaTime; //despues de definir la variable dropcounter arriba, se genera una operación que se repetirá siempre debido al request animation de abajo⏬
 
     if (dropCounter > dropInterval) { //¿cómo funciona este if ? porque dropcounter nunca es mayor que dropinterval y se sigue ejecutando siempre 
         playerDrop(); /*Funcion que  contiene la operacion para hacer que el tetramino baje cada intervalo de tiempo por el request animation */
@@ -256,8 +255,8 @@ function playerReset () { //funcion creada para enviar un nueva ficha cuando la 
     player.pos.y = 0;
 
     
-    if (collide(grid,player)){//todo qué funcionalidad se necesita para que salga un alert y seleccione si desea continuar
-        //cuando pierde 
+    if (collide(grid,player)){//all what functionality is needed to get an alert and select if you want to continue
+        //when you loose
         alert ("has perdido");        
         grid.forEach(row => row.fill(0));
         player.score =0;
@@ -270,22 +269,22 @@ function playerReset () { //funcion creada para enviar un nueva ficha cuando la 
 
 
 
-function playerMove (direction){ //se limita el movimiento 
+function playerMove (direction){ //the movement is limited
     player.pos.x += direction; // 
-    if (collide(grid,player)){ //si colisiona 
+    if (collide(grid,player)){ //if it collides
         player.pos.x -= direction;        
     }
 }
 
-function playerRotate () {//funciona para lo ratacion de las fichas
-    const pos = player.pos.x; //declarar costante de posicion sin actualizar 
+function playerRotate () {//THis function works for the rotation of the chips
+    const pos = player.pos.x; //declare positional constant without updating
 
-    let offset =1;//declara la variable que guarde la posicion inicial 
+    let offset =1;//declare the variable that saves the initial position
     rotate (player.matriz);
 
-    while (collide (grid,player)){// si hay colisión, se le suma 1 a x lo que hace que nunca supere el limite vertical de lado y lado 
+    while (collide (grid,player)){// if there is a collision, 1 is added to x which means that it never exceeds the vertical limit of side and side
         player.pos.x += offset;
-        offset = -(offset + (offset > 0 ? 1:-1)); //comprobar si offset es mayor que 0, dunciona para el limite derecho ya que devuelve la ficha si intenta superar el borde restandiole uno 
+        offset = -(offset + (offset > 0 ? 1:-1)); //check if the offset is greater than 0, it works for the right limit since it returns the tile if it tries to exceed the border by subtracting one
         if (offset > player.matriz[0].length){//
             rotate(player.matriz);
             player.pos.x = pos;
@@ -294,7 +293,7 @@ function playerRotate () {//funciona para lo ratacion de las fichas
     }
 }
 
-function rotate (matriz){ //cambia la posicion de la ficha 
+function rotate (matriz){ //Change the position of the token
     for(let y =0; y < matriz.length; ++y ){
         for (let x=0; x<y; ++x) {
             [matriz [x][y] , matriz [y][x]] = [matriz [y][x], matriz[x][y]];           
@@ -306,9 +305,9 @@ function rotate (matriz){ //cambia la posicion de la ficha
 
 
 
-//cómo se vincula los movimientos de las teclas con el juego
+//This is how you can to link the movement of the keys whith the game
 document.addEventListener("keydown", event => {
-    if (event.key==="ArrowDown") { //por medio de condiciones se validara qué tecla pulsa el usuario y se le aumentará la posición en eje x y y 
+    if (event.key==="ArrowDown") { //Through conditions, it will be validated which key the user presses and the position in x and y axis will be changed.
         playerDrop();
     }else if (event.key=== "ArrowLeft"){
         playerMove(-1);
@@ -319,7 +318,7 @@ document.addEventListener("keydown", event => {
     }
 });
 
-
+//SElects the buttons that we create for asociate to the wanted movement
 movLeft.addEventListener("click", event => {
     playerMove(-1);
 });
